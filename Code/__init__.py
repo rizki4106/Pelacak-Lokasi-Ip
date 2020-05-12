d6c0af6291cbb937f8e05a4b9a71cca8ae8eb255
rizki4106 <rizkimaulana4106@gmail.com>
2020-05-12 09:38:36 +0700

    fix variable error

--- Code/__init__.py
+++ Code/__init__.py
@@ -7,20 +7,22 @@ class Deteksi:
         ip = str(input('Masukan alamat ip > '))
         url = "https://ipapi.co/"+ ip +"/json/"
         req = requests.get(url)
+        data = req.json()
         try:
             if req.status_code == 200:
 
-                negara = str(req.json()['country_name'])
-                kab = str(req.json()['region'])
-                kota = str(req.json()['city'])
-                bendera = str(req.json()['postal'])
-                latitude = str(req.json()['latitude'])
-                longitude = str(req.json()['longitude'])
+                negara = data['country_name']
+                kab = data['region']
+                kota = data['city']
+                bendera = data['postal']
+                latitude = data['latitude']
+                longitude = data['longitude']
                 print('\nNegara : {} \nProvinsi : {} \nKota : {}\nPostal : {}\nLatitude : {}\nLongitude : {}'.format(negara, kab, kota, bendera, latitude, longitude))
             else:
                 print('Gagal mengambil data')
-        except:
+        except IOError:
             print('Data gagal di ambil')
+            print(IOError)
 
 
 if __name__ != '__main__':
